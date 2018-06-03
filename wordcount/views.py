9647927626e9c8670567f848b4ec4a6dd0af2d72
Ron Stapleton <rstapleton6@gmail.com>
2018-06-03 10:26:38 +0800

    First commit after completing the wordcount project lessons

---
+++ wordcount/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+def homepage(request):
+    # return HttpResponse('Hello - you have reached home page')
+    return render(request, 'home.html',)
+
+def about(request):
+    return render(request, 'about.html')
+
+def count(request):
+    fulltext = request.GET['fulltext']
+
+    wordlist = fulltext.split()
+
+    count = len(wordlist)
+
+    worddictionary = {}
+
+    for word in wordlist:
+        if word in worddictionary:
+            # increase value
+            worddictionary[word] += 1
+
+        else:
+            # add to dictionary
+            worddictionary[word] = 1
+
+    sortedwords = sorted(worddictionary.items(), key=operator.itemgetter(1), reverse=True)
+
+    return render(request, 'count.html', {'fulltext':fulltext, 'count':count, 'sortedwords':sortedwords})
